ac9ea3434a4799b6aa258a3bb64292499e42cf77
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-05-21 13:28:32 +0200

    initiating version tracking with custom spec

--- Sources/app.swift
+++ Sources/app.swift
@@ -3,20 +3,17 @@ import plate
 
 @main
 struct ResponderApp: App {
-    // @StateObject private var mailerViewModel: MailerViewModel
-    // @StateObject private var variableStore: VariableStore
-    // @StateObject private var invoiceVm: MailerAPIInvoiceVariablesViewModel
-
     @StateObject private var mailerViewModel = MailerViewModel()
     @StateObject private var invoiceVm = MailerAPIInvoiceVariablesViewModel()
 
+    let buildSpecification = BuildSpecification(
+      version: BuildVersion(major: 2, minor: 2, patch: 1),
+      name: "Responder",
+      author: "Levi Ouwendijk",
+      description: ""
+    )
+
     init() {
-        // _mailerViewModel = StateObject(wrappedValue: MailerViewModel())
-
-        // let vs = VariableStore()
-        // _variableStore   = StateObject(wrappedValue: vs)
-        // _invoiceVm       = StateObject(wrappedValue: MailerAPIInvoiceVariablesViewModel(store: vs))
-
         NSWindow.allowsAutomaticWindowTabbing = false
     }
 
@@ -24,29 +21,38 @@ struct ResponderApp: App {
 
     var body: some Scene {
         WindowGroup {
-            TabView(selection: $selectedTab) {
-                Responder()
-                  .environmentObject(mailerViewModel)
-                  .environmentObject(invoiceVm)
-                  .tabItem {
-                    Label("Responder", systemImage: "paperplane.fill")
-                  }
-                  .tag(0)
+            VStack {
+                TabView(selection: $selectedTab) {
+                    Responder()
+                      .environmentObject(mailerViewModel)
+                      .environmentObject(invoiceVm)
+                      .tabItem {
+                        Label("Responder", systemImage: "paperplane.fill")
+                      }
+                      .tag(0)
 
-                MessageMakerView()
-                  .environmentObject(mailerViewModel)
-                  .tabItem {
-                    Label("WA", systemImage: "message.fill")
-                  }
-                  .tag(1)
+                    MessageMakerView()
+                      .environmentObject(mailerViewModel)
+                      .tabItem {
+                        Label("WA", systemImage: "message.fill")
+                      }
+                      .tag(1)
 
-                MailerStandardOutput()
-                  .environmentObject(mailerViewModel)
-                  .tabItem {
-                    Label("stdout", systemImage: "terminal.fill")
-                  }
-                  .tag(2)
+                    MailerStandardOutput()
+                      .environmentObject(mailerViewModel)
+                      .tabItem {
+                        Label("stdout", systemImage: "terminal.fill")
+                      }
+                      .tag(2)
+                }
+
+                BuildInformation(
+                    specification: buildSpecification,
+                    alignment: .center,
+                    display: [.version]
+                )
             }
         }
     }
 }
+
