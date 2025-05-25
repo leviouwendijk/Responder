9b29c2129f594aee10afe3e0632a1ee40db8afe7
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-05-25 11:50:34 +0200

    adding build object

--- Sources/app.swift
+++ Sources/app.swift
@@ -6,15 +6,29 @@ struct ResponderApp: App {
     @StateObject private var mailerViewModel = MailerViewModel()
     @StateObject private var invoiceVm = MailerAPIInvoiceVariablesViewModel()
 
-    let buildSpecification = BuildSpecification(
-      version: BuildVersion(major: 2, minor: 4, patch: 1),
-      name: "Responder",
-      author: "Levi Ouwendijk",
-      description: ""
-    )
+    let buildSpecification: BuildSpecification
+
+    // let buildSpecification = BuildSpecification(
+    //   version: BuildVersion(major: 2, minor: 4, patch: 1),
+    //   name: "Responder",
+    //   author: "Levi Ouwendijk",
+    //   description: ""
+    // )
 
     init() {
         NSWindow.allowsAutomaticWindowTabbing = false
+
+        do {
+            buildSpecification = try BuildSpecification(fromPkl: URL(fileURLWithPath: "build-object.pkl"))
+        } catch {
+            print("Failed to load PKL, using default:", error)
+            buildSpecification = BuildSpecification(
+                version: BuildVersion(major: 2, minor: 4, patch: 1),
+                name: "Responder",
+                author: "Levi Ouwendijk",
+                description: ""
+            )
+        }
     }
 
     @State private var selectedTab: Int = 0
