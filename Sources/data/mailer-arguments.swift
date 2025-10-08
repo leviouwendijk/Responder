4767869c0d2900e75ba9d671a4a23eaf87e39f49
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-08 12:16:47 +0200

    fixed issue where '--expired' flag wasn't properly added in mailer cli constructor

--- Sources/data/mailer-arguments.swift
+++ Sources/data/mailer-arguments.swift
@@ -1,107 +1,111 @@
-import Foundation
-import Interfaces
-import plate
+// import Foundation
+// import Interfaces
+// import plate
 
-struct MailerCLIStateVariables {
-    let invoiceId: String
-    let fetchableCategory: String
-    let fetchableFile: String
-    let finalEmail: String
-    let finalSubject: String
-    let finalHtml: String
-    let includeQuote: Bool
-} 
+// struct MailerCLIStateVariables {
+//     let invoiceId: String
+//     let fetchableCategory: String
+//     let fetchableFile: String
+//     let finalEmail: String
+//     let finalSubject: String
+//     let finalHtml: String
+//     let includeQuote: Bool
+// } 
 
-enum MailerCLIArgError: Error {
-    case missingArgumentComponents
-}
+// enum MailerCLIArgError: Error {
+//     case missingArgumentComponents
+// }
 
-struct MailerCLIArguments {
-    let client: String
-    let email: String
-    let dog: String
-    let route: MailerAPIRoute?
-    let endpoint: MailerAPIEndpoint?
-    let availabilityJSON: String?
-    let needsAvailability: Bool
-    let stateVariables: MailerCLIStateVariables
+// struct MailerCLIArguments {
+//     let client: String
+//     let email: String
+//     let dog: String
+//     let route: MailerAPIRoute?
+//     let endpoint: MailerAPIEndpoint?
+//     let availabilityJSON: String?
+//     let needsAvailability: Bool
+//     let stateVariables: MailerCLIStateVariables
 
-    func string(_ includeBinaryName: Bool = false) throws -> String {
-        guard let r = route, let e = endpoint else {
-            throw MailerCLIArgError.missingArgumentComponents
-        }
-
-        var components: [String] = []
-
-        if includeBinaryName {
-            components.insert("mailer", at: 0)
-        }
-
-        switch r {
-        case .invoice:
-            components.append("invoice \(stateVariables.invoiceId) --responder")
-            if e == .expired {
-                components.append("--expired")
-            }
-
-        case .template: 
-            components.append("template-api \(stateVariables.fetchableCategory) \(stateVariables.fetchableFile)")
-
-        case .custom:
-            components.append("custom-message --email \"\(stateVariables.finalEmail)\" --subject \"\(stateVariables.finalSubject)\" --body \"\(stateVariables.finalHtml)\"")
-
-            if  stateVariables.includeQuote {
-                components.append(" --quote")
-            }
-
-        default:
-            components.append(r.rawValue)
-            components.append("--client \"\(client)\"")
-            components.append("--email \"\(email)\"")
-            components.append("--dog \"\(dog)\"")
-
-            if !(e == .issue || e == .confirmation || e == .review) {
-                components.append("--\(e.rawValue)")
-            }
-
-            if needsAvailability {
-                components.append("--availability-json '\(availabilityJSON ?? "")'")
-            }
-        }
-
-        let compacted = components.compactMap { $0 }
-        return compacted.joined(separator: " ")
-    }
-}
-
-public func executeMailerCLI(_ arguments: String) throws {
-    do {
-        let home = Home.string()
-        let process = Process()
-        process.executableURL = URL(fileURLWithPath: "/bin/zsh") // Use Zsh directly
-        process.arguments = ["-c", "source ~/dotfiles/.vars.zsh && \(home)/sbm-bin/mailer \(arguments)"]
+//     func string(_ includeBinaryName: Bool = false) throws -> String {
+//         guard let r = route, let e = endpoint else {
+//             throw MailerCLIArgError.missingArgumentComponents
+//         }
         
-        let outputPipe = Pipe()
-        let errorPipe = Pipe()
-        process.standardOutput = outputPipe
-        process.standardError = errorPipe
+//         var components: [String] = []
 
-        try process.run()
-        process.waitUntilExit()
+//         if includeBinaryName {
+//             components.insert("mailer", at: 0)
+//         }
 
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        let outputString = String(data: outputData, encoding: .utf8) ?? ""
-        let errorString = String(data: errorData, encoding: .utf8) ?? ""
+//         switch r {
+//         case .invoice:
+//             components.append("invoice \(stateVariables.invoiceId) --responder")
+//             if e.base == .expired {
+//             // if .expired ~= e {
+//                 components.append("--expired")
+//             }
 
-        if process.terminationStatus == 0 {
-            print("mailer executed successfully:\n\(outputString)")
-        } else {
-            print("Error running mailer:\n\(errorString)")
-            throw NSError(domain: "mailer", code: Int(process.terminationStatus), userInfo: [NSLocalizedDescriptionKey: errorString])
-        }
-    } catch {
-        print("Error running commands: \(error.localizedDescription)")
-        throw error
-    }
-}
+//         case .template: 
+//             components.append("template-api \(stateVariables.fetchableCategory) \(stateVariables.fetchableFile)")
+
+//         case .custom:
+//             components.append("custom-message --email \"\(stateVariables.finalEmail)\" --subject \"\(stateVariables.finalSubject)\" --body \"\(stateVariables.finalHtml)\"")
+
+//             if  stateVariables.includeQuote {
+//                 components.append(" --quote")
+//             }
+
+//         default:
+//             components.append(r.rawValue)
+//             components.append("--client \"\(client)\"")
+//             components.append("--email \"\(email)\"")
+//             components.append("--dog \"\(dog)\"")
+
+//             if !(e == .issue || e == .confirmation || e == .review) {
+//                 components.append("--\(e.rawValue)")
+//             }
+
+//             if needsAvailability {
+//                 components.append("--availability-json '\(availabilityJSON ?? "")'")
+//             }
+//         }
+
+//         let compacted = components.compactMap { $0 }
+//         return compacted.joined(separator: " ")
+//     }
+// }
+
+// public func executeMailerCLI(_ arguments: String) throws {
+//     do {
+//         print("Executing mailer cli with arguments:")
+//         printi(arguments)
+
+//         let home = Home.string()
+//         let process = Process()
+//         process.executableURL = URL(fileURLWithPath: "/bin/zsh") // Use Zsh directly
+//         process.arguments = ["-c", "source ~/dotfiles/.vars.zsh && \(home)/sbm-bin/mailer \(arguments)"]
+        
+//         let outputPipe = Pipe()
+//         let errorPipe = Pipe()
+//         process.standardOutput = outputPipe
+//         process.standardError = errorPipe
+
+//         try process.run()
+//         process.waitUntilExit()
+
+//         let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+//         let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+//         let outputString = String(data: outputData, encoding: .utf8) ?? ""
+//         let errorString = String(data: errorData, encoding: .utf8) ?? ""
+
+//         if process.terminationStatus == 0 {
+//             print("mailer executed successfully:\n\(outputString)")
+//         } else {
+//             print("Error running mailer:\n\(errorString)")
+//             throw NSError(domain: "mailer", code: Int(process.terminationStatus), userInfo: [NSLocalizedDescriptionKey: errorString])
+//         }
+//     } catch {
+//         print("Error running commands: \(error.localizedDescription)")
+//         throw error
+//     }
+// }
